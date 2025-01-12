4c3e9ddb082d2d30350130a75bfa209ab46c7c89
Mushfiq1010 <151173437+Mushfiq1010@users.noreply.github.com>
2025-01-12 14:03:21 +0600

    Ui done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,27 @@
 import { useState } from 'react'
-
-
+import { Route,Routes } from 'react-router-dom'
+import HomePage from './pages/home/HomePage'
+import LoginPage from './pages/auth/login/LoginPage'
+import SignUpPage from './pages/auth/signup/SignUpPage'
+import Sidebar from './components/common/SideBar'
+import RightPanel from './components/common/RightPanel'
+import NotificationPage from './pages/notification/NotificationPage'
+import ProfilePage from './pages/profile/ProfilePage'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-   <button className="btn">Button</button>
-<button className="btn btn-neutral">Neutral</button>
-<button className="btn btn-primary">Primary</button>
-<button className="btn btn-secondary">Secondary</button>
-<button className="btn btn-accent">Accent</button>
-<button className="btn btn-ghost">Ghost</button>
-<button className="btn btn-link">Link</button>
-    </>
+    <div className='flex max-w-6xl mx-auto'>
+      <Sidebar/>  {/*common component if outside Routes*/}
+      <Routes>
+      <Route path="/" element={<HomePage />}/>
+      <Route path="/login" element={<LoginPage />}/>
+      <Route path="/signup" element={<SignUpPage />}/>
+      <Route path="/notifications" element={<NotificationPage />}/>
+      <Route path="/profile/:username" element={<ProfilePage />}/>
+      </Routes>
+   <RightPanel/>
+    </div>
   )
 }
 
